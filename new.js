63a80a1e650af9ae52506a95d5f6b77c24d9b104
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-04-09 00:43:34 +0100

    Update new.js

--- new.js
+++ new.js
@@ -1,29 +1,29 @@
-const { makeid } = require('./id')
-const id = makeid()
-const fs = require('fs')
-const pino = require('pino')
-const { default: makeWASocket, Browsers, delay, useMultiFileAuthState, BufferJSON, fetchLatestBaileysVersion, PHONENUMBER_MCC, DisconnectReason, makeInMemoryStore, jidNormalizedUser, makeCacheableSignalKeyStore } = require("@whiskeysockets/baileys")
-const Pino = require("pino")
-const NodeCache = require("node-cache")
-const chalk = require("chalk")
-const readline = require("readline")
-const { parsePhoneNumber } = require("libphonenumber-js")
+const { makeid } = require('./id');
+const id = makeid();
+const fs = require('fs');
+const pino = require('pino');
+const axios = require('axios'); // Add axios for making HTTP requests
+const { default: makeWASocket, Browsers, delay, useMultiFileAuthState, BufferJSON, fetchLatestBaileysVersion, PHONENUMBER_MCC, DisconnectReason, makeInMemoryStore, jidNormalizedUser, makeCacheableSignalKeyStore } = require("@whiskeysockets/baileys");
+const Pino = require("pino");
+const NodeCache = require("node-cache");
+const chalk = require("chalk");
+const readline = require("readline");
+const { parsePhoneNumber } = require("libphonenumber-js");
 const archiver = require('archiver');
 
-let phoneNumber = "2348114860536"
+let phoneNumber = "2348114860536";
 
-const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code")
-const useMobile = process.argv.includes("--mobile")
+const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code");
+const useMobile = process.argv.includes("--mobile");
 
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
-const question = (text) => new Promise((resolve) => rl.question(text, resolve))
-
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const question = (text) => new Promise((resolve) => rl.question(text, resolve));
 
 async function qr() {
     //------------------------------------------------------
-    let { version, isLatest } = await fetchLatestBaileysVersion()
-    const { state, saveCreds } = await useMultiFileAuthState('./session/' + id)
-    const msgRetryCounterCache = new NodeCache() // for retry message, "waiting message"
+    let { version, isLatest } = await fetchLatestBaileysVersion();
+    const { state, saveCreds } = await useMultiFileAuthState('./session/' + id);
+    const msgRetryCounterCache = new NodeCache(); // for retry message, "waiting message"
     const XeonBotInc = makeWASocket({
         logger: pino({ level: 'silent' }),
         printQRInTerminal: !pairingCode, // popping up QR in terminal log
@@ -35,56 +35,55 @@ async function qr() {
         markOnlineOnConnect: true, // set false for offline
         generateHighQualityLinkPreview: true, // make high preview link
         getMessage: async (key) => {
-            let jid = jidNormalizedUser(key.remoteJid)
-            let msg = await store.loadMessage(jid, key.id)
+            let jid = jidNormalizedUser(key.remoteJid);
+            let msg = await store.loadMessage(jid, key.id);
 
-            return msg?.message || ""
+            return msg?.message || "";
         },
         msgRetryCounterCache, // Resolve waiting messages
         defaultQueryTimeoutMs: undefined, // for this issues https://github.com/WhiskeySockets/Baileys/issues/276
-    })
-
+    });
 
     // login use pairing code
     // source code https://github.com/WhiskeySockets/Baileys/blob/master/Example/example.ts#L61
     if (pairingCode && !XeonBotInc.authState.creds.registered) {
-        if (useMobile) throw new Error('Cannot use pairing code with mobile api')
+        if (useMobile) throw new Error('Cannot use pairing code with mobile api');
 
-        let phoneNumber
+        let phoneNumber;
         if (!!phoneNumber) {
-            phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
+            phoneNumber = phoneNumber.replace(/[^0-9]/g, '');
 
             if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
-                console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +2348114860536")))
-                process.exit(0)
+                console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +2348114860536")));
+                process.exit(0);
             }
         } else {
-            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFor example: +2348114860536 : `)))
-            phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
+            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFor example: +2348114860536 : `)));
+            phoneNumber = phoneNumber.replace(/[^0-9]/g, '');
 
             // Ask again when entering the wrong number
             if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
-                console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +2348114860536")))
+                console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +2348114860536")));
 
-                phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFor example: +2348114860536 : `)))
-                phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
-                rl.close()
+                phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFor example: +2348114860536 : `)));
+                phoneNumber = phoneNumber.replace(/[^0-9]/g, '');
+                rl.close();
             }
         }
 
         setTimeout(async () => {
-            let code = await XeonBotInc.requestPairingCode(phoneNumber)
-            code = code?.match(/.{1,4}/g)?.join("-") || code
-            console.log(chalk.black(chalk.bgGreen(`Your Pairing Code : `)), chalk.black(chalk.white(code)))
-        }, 3000)
+            let code = await XeonBotInc.requestPairingCode(phoneNumber);
+            code = code?.match(/.{1,4}/g)?.join("-") || code;
+            console.log(chalk.black(chalk.bgGreen(`Your Pairing Code : `)), chalk.black(chalk.white(code)));
+        }, 3000);
     }
     //------------------------------------------------------
     XeonBotInc.ev.on("connection.update", async (s) => {
-        const { connection, lastDisconnect } = s
+        const { connection, lastDisconnect } = s;
         if (connection == "open") {
-            await delay(1000 * 10)
+            await delay(1000 * 10);
             await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: '*thanks for choosing alpha-md*\n*your sesssionid will be sent in 20 seconds please wait..*\n*have a great day ahead*' });
-            await delay(1000 * 20)
+            await delay(1000 * 20);
             const folderPath = `./session/${id}/`;
             const randomIdn = id;
             const randomId = 'alpha~' + randomIdn;
@@ -94,7 +93,23 @@ async function qr() {
             });
             output.on('close', async () => {
                 console.log('Zip file created successfully.');
-                // You can remove the MongoDB related code here
+
+                // Upload the zip file and ID to the server
+                const formData = new FormData();
+                formData.append('file', fs.createReadStream(`creds_${randomId}.zip`));
+                formData.append('id', randomId);
+
+                try {
+                    const response = await axios.post('http://localhost:3000/upload', formData, {
+                        headers: {
+                            'Content-Type': 'multipart/form-data',
+                        },
+                    });
+                    console.log('Server response:', response.data);
+                } catch (error) {
+                    console.error('Error uploading file to server:', error);
+                }
+
                 XeonBotInc.groupAcceptInvite("BGWpp9qySw81CGrqRM3ceg");
                 const xeonses = await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: randomId });
                 await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `*ᴅᴇᴀʀ ᴜsᴇʀ ᴛʜɪs ɪs ʏᴏᴜʀ sᴇssɪᴏɴ ɪᴅ*\n*◕ ⚠️ ᴘʟᴇᴀsᴇ ᴅᴏ ɴᴏᴛ sʜᴀʀᴇ ᴛʜɪs ᴄᴏᴅᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ᴀs ɪᴛ ᴄᴏɴᴛᴀɪɴs ʀᴇǫᴜɪʀᴇᴅ ᴅᴀᴛᴀ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ᴄᴏɴᴛᴀᴄᴛ ᴅᴇᴛᴀɪʟs ᴀɴᴅ ᴀᴄᴄᴇss ʏᴏᴜʀ ᴡʜᴀᴛsᴀᴘᴘ*` }, { quoted: xeonses });
@@ -112,7 +127,7 @@ async function qr() {
             archive.pipe(output);
             archive.directory(folderPath, false);
             archive.finalize();
-            await delay(1000 * 2)
+            await delay(1000 * 2);
             process.exit(0);
         }
         if (
@@ -121,22 +136,23 @@ async function qr() {
             lastDisconnect.error &&
             lastDisconnect.error.output.statusCode != 401
         ) {
-            qr()
+            qr();
         }
-    })
-    XeonBotInc.ev.on('creds.update', saveCreds)
-    XeonBotInc.ev.on("messages.upsert", () => { })
+    });
+    XeonBotInc.ev.on('creds.update', saveCreds);
+    XeonBotInc.ev.on("messages.upsert", () => { });
 }
-qr()
+
+qr();
 
 process.on('uncaughtException', function (err) {
-    let e = String(err)
-    if (e.includes("conflict")) return
-    if (e.includes("not-authorized")) return
-    if (e.includes("Socket connection timeout")) return
-    if (e.includes("rate-overlimit")) return
-    if (e.includes("Connection Closed")) return
-    if (e.includes("Timed Out")) return
-    if (e.includes("Value not found")) return
-    console.log('Caught exception: ', err)
-})
+    let e = String(err);
+    if (e.includes("conflict")) return;
+    if (e.includes("not-authorized")) return;
+    if (e.includes("Socket connection timeout")) return;
+    if (e.includes("rate-overlimit")) return;
+    if (e.includes("Connection Closed")) return;
+    if (e.includes("Timed Out")) return;
+    if (e.includes("Value not found")) return;
+    console.log('Caught exception: ', err);
+});
